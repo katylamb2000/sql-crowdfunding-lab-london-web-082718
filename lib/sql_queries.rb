20e70d40138c461a413f9aac1ccfa14019dcfa62
katy@karstaway.com <katy@karstaway.com>
2018-09-04 19:44:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,7 +53,7 @@ FROM projects, pledges
 WHERE pledges.project_id = projects.id 
 GROUP BY Projects.title 
 HAVING projects.id = pledges.project_id 
-ORDER BY SUM(amount), "
+ORDER BY SUM(amount) ";
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
