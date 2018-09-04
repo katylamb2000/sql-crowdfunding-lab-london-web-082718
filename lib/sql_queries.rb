4dc6bb96968cbf0c2ebefe35206930db3327aa55
katy@karstaway.com <katy@karstaway.com>
2018-09-04 19:36:04 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -66,7 +66,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 FROM pledges
 INNER JOIN projects
 ON pledges.project_id = projects.id 
-GROUP BY  projects.category 
+# GROUP BY  projects.category 
 HAVING projects.category = 'music'";
 end
 
