5e3a617d9f01d41628a73a888f7f37ff4e8c865f
katy@karstaway.com <katy@karstaway.com>
2018-09-04 19:34:47 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,9 +43,9 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id
-ORDER BY SUM(amount) projects.funding_goal > 1
-DESC
-GROUP BY projects.title"; 
+ORDER BY SUM(amount) projects.funding_goal > 1";
+# DESC
+# GROUP BY projects.title"; 
 end
 
 # "SELECT Projects.title, SUM(amount) - Projects.funding_goal FROM projects, pledges 
